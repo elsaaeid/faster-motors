b68fff2dbd8bbb8186778b865d6a6743804b1161
elsaaeid <saidsadaoy@gmail.com>
2022-03-05 16:37:43 +0200

    create project

---
+++ assets/js/bottom-nav.js
@@ -0,0 +1,40 @@
+// ------------------------------------------------------------------------------ //
+// Template name : bottom-nav - Multi Purpose Header
+
+
+"use strict";
+
+
+
+
+
+
+
+
+
+/**==dropdown-menu ==*/ 
+
+ // Enable click event
+                $("nav.navbar.bootsnav ul.nav").each(function(){
+                    $(".dropdown-menu", this).addClass("animated");
+                    $(".dropdown-menu", this).removeClass(getOut);
+                    
+                    // Dropdown Fade Toggle
+                    $("a.dropdown-toggle", this).off('click');
+                    $("a.dropdown-toggle", this).on('click', function (e) {
+                        e.stopPropagation();
+                        $(this).closest("li.dropdown").find(".dropdown-menu").first().stop().fadeToggle().toggleClass(getIn);
+                        $(this).closest("li.dropdown").first().toggleClass("on");                        
+                        return false;
+                    });   
+                    
+                    // Hidden dropdown action
+                    $('li.dropdown', this).each(function () {
+                        $(this).find(".dropdown-menu").stop().fadeOut();
+                        $(this).on('hidden.bs.dropdown', function () {
+                            $(this).find(".dropdown-menu").stop().fadeOut();
+                        });
+                        return false;
+                    });
+
+
---
+++ assets/js/search .js
@@ -0,0 +1,23 @@
+
+(function ($) {
+    "use strict";
+
+    
+   let index = document.querySelector('form-control');
+    let index = filter["موتوسيكلات", "سيارات جديده","سيارات مستعملة" , "قوارب"];
+
+    function search_filter(array)
+    {
+        return index.filter(element => {
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                if (typeof index.value == "string") {
+                    return index[i]
+                }
+            }
+        })
+    
+    }
+
+}
+    (jQuery));
